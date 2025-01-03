824abc7d299588f052b41c600f283d46369f0334
bera-braden-jaeger <bjaeger@bera.ai>
2025-01-03 14:50:05 -0500

    Update run.py

--- selenium/run.py
+++ selenium/run.py
@@ -1,14 +1,26 @@
+import unittest
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-import time
 
-driver = webdriver.Chrome()
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element(By.NAME, "q")
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
-driver.close()
+class TestExample(unittest.TestCase):
+    def setUp(self):
+        options = webdriver.ChromeOptions()
+        options.add_argument("--headless")  # Run tests in headless mode
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        self.driver = webdriver.Chrome(options=options)
+
+    def test_google_search(self):
+        self.driver.get("https://www.google.com")
+        self.assertIn("Google", self.driver.title)
+        search_box = self.driver.find_element(By.NAME, "q")
+        search_box.send_keys("Selenium")
+        search_box.submit()
+        # self.assertTrue("Selenium" in self.driver.page_source)
+
+    def tearDown(self):
+        self.driver.quit()
+
+if __name__ == "__main__":
+    unittest.main()
